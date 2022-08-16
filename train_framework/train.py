5bb104e16748ff6cadd2f53b05b5dd1931e0284d
MathieuJouffroy <mathieu.jouffroy@gmail.com>
2022-08-16 18:00:47 +0200

    minor changes

--- train_framework/train.py
+++ train_framework/train.py
@@ -282,11 +282,11 @@ def main():
         print(model.summary())
         print(model.inputs)
         print("----------")
-        tf.keras.utils.plot_model(model, show_shapes=True, show_dtype=True)
-        plt.show()
-        print(tf.keras.utils.plot_model(model, show_shapes=True, show_dtype=True))
-        plt.show()
-        print("----------")
+        #tf.keras.utils.plot_model(model, show_shapes=True, show_dtype=True)
+        #plt.show()
+        #print(tf.keras.utils.plot_model(model, show_shapes=True, show_dtype=True))
+        #plt.show()
+        #print("----------")
         for layer in model.layers:
             if 'input' in layer.__class__.__name__:
                 print(layer.name, layer.input_shape)
