8838199933f9bf326503ffb4d60089dd0776a8eb
MathieuJouffroy <mathieu.jouffroy@gmail.com>
2022-09-20 15:37:54 +0200

    gradio with convnext

--- train_framework/app.py
+++ train_framework/app.py
@@ -7,18 +7,55 @@ import requests
 from metrics import f1_m
 import json 
 from tensorflow import keras 
+#from tensorflow.keras.applications.convnext import LayerScale
 
 ### GRADIO INTERFACE WITH PREDICTION CAM / GRADCAM / SALIENCY MAPS
 dependencies = {
     'f1_m': f1_m,
 }
 
+class LayerScale(tf.keras.layers.Layer):
+    """Layer scale module.
+    References:
+      - https://arxiv.org/abs/2103.17239
+    Args:
+      init_values (float): Initial value for layer scale. Should be within
+        [0, 1].
+      projection_dim (int): Projection dimensionality.
+    Returns:
+      Tensor multiplied to the scale.
+    """
+
+    def __init__(self, init_values, projection_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.init_values = init_values
+        self.projection_dim = projection_dim
+
+    def build(self, input_shape):
+        self.gamma = tf.Variable(
+            self.init_values * tf.ones((self.projection_dim,))
+        )
+
+    def call(self, x):
+        return x * self.gamma
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "init_values": self.init_values,
+                "projection_dim": self.projection_dim,
+            }
+        )
+        return config
+
 with open("../resources/label_maps/diseases_label_map.json") as f:
     CLASS_INDEX = json.load(f)
 
 @tf.function
 def resize_img(img):
-    img = tf.image.resize(img, (128, 128))
+    img = tf.image.resize(img, (224, 224))
+    #img = tf.image.resize(img, (128, 128))
     return img
 
 #@tf.function
@@ -36,47 +73,40 @@ def classify_image(input, label, model):
     input = tf.expand_dims(input, 0)  # Create batch axis
     input = tf.cast(input, tf.float32)
     input = resize_img(input)
-    #label = encode_categorical(label, 38)
     print(input.shape)
     
-    #if model == 'CVT':
-    #    #input = tf.keras.applications.mobilenet_v2.preprocess_input(input)
-    #    clf = tf.keras.applications.MobileNetV2()
-    #elif model == 'VIT':
-    #    #input = tf.keras.applications.mobilenet_v2.preprocess_input(input)
-    #    clf = tf.keras.applications.MobileNetV2()
+
     if model == 'EfficientNetV2B3':
-        clf =  tf.keras.models.load_model("wandb/run-20220915_175411-1tbtjv3t/files/model-best.h5",custom_objects=dependencies)
-    elif model == 'InceptionResnetV2':
-        clf =  tf.keras.models.load_model("Best_models/InceptionResNetV2_dropout_/model-best.h5",custom_objects=dependencies)
+        clf =  tf.keras.models.load_model("Best_models/pret_EfficientNetV2B3_dropout/model-best.h5",custom_objects=dependencies)
+    elif model == 'ConvNext':
+        clf =  tf.keras.models.load_model("Best_models/ConvNext/model-best.h5",custom_objects={'f1_m': f1_m, 'LayerScale':LayerScale})
     elif model == 'InceptionV3':
         clf =  tf.keras.models.load_model("../resources/experiments/comp-top-k/InceptionV3_dropout_16:09:2022_07:31:58",custom_objects=dependencies)
     elif model == 'DenseNet201':
         #input = tf.keras.applications.mobilenet_v2.preprocess_input(input)
-        clf = tf.keras.models.load_model("wandb/run-20220916_150125-2azy63ub/files/model-best.h5", custom_objects=dependencies)
+        clf = tf.keras.models.load_model("Best_models/pret_DenseNet201/model-best.h5", custom_objects=dependencies)
     elif model == 'ResNet50V2':
-        clf = tf.keras.models.load_model("wandb/run-20220916_133658-1w7qi5vq/files/model.h5",custom_objects=dependencies)
+        clf = tf.keras.models.load_model("Best_models/pret_ResNet50V2/model-best.h5",custom_objects=dependencies)
 
 
-    #y_probs = clf(input)
-    clf = clf(input, training=False)
-    print(clf)
-    y_probs = clf(input)
+    
+    y_probs = clf.predict(input)
+    y_pred = np.argmax(y_probs, axis=-1)
+    pred_label_names = [CLASS_INDEX[str(y)] for y in y_pred]
+    pred = pred_label_names[0]
+    print(pred)
+    return pred
     #y_pred = np.argmax(y_probs, axis=-1)
     #pred_label_names = [CLASS_INDEX[str(y)] for y in y_pred]
     #pred = pred_label_names[0]
     #return pred
-    print(y_probs)
-    prediction = y_probs.flatten()
-    confidences = {CLASS_INDEX[i]: float(prediction[i]) for i in range(38)}
-    return confidences
 
 demo = gr.Interface(
             fn=classify_image, 
             inputs=[
               gr.Image(shape=(128, 128)),
 			  gr.Textbox(value='Apple Healthy', label='label'),
-              gr.Dropdown(choices=['EfficientNetV2B3','InceptionV3','InceptionResnetV2', 'ResNet50V2'], type="value", label='model'),
+              gr.Dropdown(choices=['EfficientNetV2B3','InceptionV3','ConvNext', 'ResNet50V2', 'DenseNet201'], type="value", label='model'),
               #gr.Checkbox(label="Remove Background"),
               ],
             outputs=gr.Label(num_top_classes=3),
